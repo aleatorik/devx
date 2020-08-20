fc2cc9994b2017504320b7f404da8ed57798b946
daseulsong <vates0328@gmail.com>
2020-08-20 21:57:45 +0900

    마우스 호버 시 상품 이미지 변환 구현

--- src/Pages/shop/Components/product/Product.js
+++ src/Pages/shop/Components/product/Product.js
@@ -6,30 +6,37 @@ import "./Product.scss";
 class Product extends Component {
   render() {
     return (
-      <div className="product_wrapper">
+      <div className="product__wrapper">
         <div className="productContainer">
           <div className="productItem">
-            <Link className="productItem__wrapper" to="/detail">
+            <Link className="productItem__wrapper" to="/shop/detail">
               <div className="imgFixedCtn">
                 <img
-                  alt="tshirt"
-                  data-widths="[200,300,400,500,600,800,900]"
-                  data-sizes="auto"
-                  src="/images/shop/tshirt.png"
+                  alt="tshirt-front"
+                  className="productItem__img img--fadeIn"
+                  src={this.props.frontSrc}
+                  //   src={`http://localhost:3000/shop/${this.props.id}?set=set2&size=435x435`}
                 />
-                <span className="imageLoader"></span>
+                <img
+                  alt="tshirt-back"
+                  className="productItem__img back-img img--fadeIn"
+                  src={this.props.backSrc}
+                />
+                {/* src={`http://localhost:3000/shop/${this.props.id}?set=set2&size=435x435`} */}
               </div>
             </Link>
             <div className="productItemInfo">
               <div className="productItem__title Heading">
-                <Link className="productItem__wrapper" to="/detail">
+                <Link className="productItem__wrapper" to="/shop/detail">
                   Men's Demo 2 Mission T-Shirt
+                  {this.props.name}
                 </Link>
               </div>
               <div className="productItem__priceList Heading">
                 <span className="productItem__price price">
                   <span className="money" data-product-id="123456789">
                     $30.00
+                    {this.props.price}
                   </span>
                 </span>
               </div>
