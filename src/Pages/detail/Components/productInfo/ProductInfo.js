cf8fc46fb2da8f0073b9d52c649d0c985c99814f
daseulsong <vates0328@gmail.com>
2020-08-23 01:41:53 +0900

    [송다슬] Add: 우측 Info섹션 레이아웃 작업

--- src/Pages/detail/Components/productInfo/ProductInfo.js
+++ src/Pages/detail/Components/productInfo/ProductInfo.js
@@ -6,7 +6,7 @@ class ProductInfo extends Component {
   render() {
     return (
       <div>
-        <div className="product__infoWrapper">
+        <div className="ProductInfo infoWrapper">
           <div className="product__info">
             <div className="container">
               <div className="productMeta">
@@ -22,40 +22,119 @@ class ProductInfo extends Component {
                   <div className="productMeta__description--empty">
                     <br></br>
                   </div>
-                  <form method="post" className="productForm">
-                    <div className="productForm__variants">
-                      <div className="productForm__option">
-                        <button type="button" className="productForm__item">
-                          <span className="productForm__optionName">
-                            <span className="productForm__selectedValue"></span>
-                          </span>
-                          <svg className="icon icon--selected-arrow"></svg>
-                        </button>
-                      </div>
-                      <div className="productForm__option"></div>
-                    </div>
-                    <button
-                      type="submit"
-                      className="productForm__addToCart button"
-                    >
-                      <span>Add to cart</span>
-                    </button>
-                  </form>
-                  <div className="product__quickNav">
-                    <div className="product__quickNavWrapper">
-                      <Link className="Heading">More information</Link>
-                      <svg className="icon icon--selet-arrow-right">
-                        <path
-                          d="M1.5 1.5l8 7.5-8 7.5"
-                          stroke-width="2"
-                          stroke="currentColor"
+                </div>
+              </div>
+              <form method="post" className="productForm">
+                <input type="hidden" name="form_type" value="product"></input>
+                <input type="hidden" name="utf8" value="✓"></input>
+                <div className="productForm__variants">
+                  <div className="productForm__option">
+                    <button type="button" className="productForm__item">
+                      <span className="productForm__optionName">
+                        "Color: "
+                        <span className="productForm__selectedValue"></span>
+                      </span>
+                      {/* <svg
+                        className="icon icon--selected-arrow"
+                        role="presentation"
+                        viewBox="0 0 19 12"
+                      >
+                        <polyline
                           fill="none"
+                          stroke="currentColor"
+                          points="17 2 9.5 10 2 2"
                           fill-rule="evenodd"
+                          stroke-width="2"
                           stroke-linecap="square"
-                        ></path>
-                      </svg>
+                        ></polyline>
+                      </svg> */}
+                    </button>
+                  </div>
+                  <div className="productForm__option">
+                    <button type="button" className="productForm__item">
+                      <span className="productForm__optionName">
+                        "Color: "
+                        <span className="productForm__selectedValue"></span>
+                      </span>
+                      {/* <svg
+                        className="icon icon--selected-arrow"
+                        role="presentation"
+                        viewBox="0 0 19 12"
+                      >
+                        <polyline
+                          fill="none"
+                          stroke="currentColor"
+                          points="17 2 9.5 10 2 2"
+                          fill-rule="evenodd"
+                          stroke-width="2"
+                          stroke-linecap="square"
+                        ></polyline>
+                      </svg> */}
+                    </button>
+                  </div>
+                  <div className="productForm__quantitySelector">
+                    <div className="quantitySelector quantitySelector--large">
+                      <span className="quantitySelector__btn decrease-quantity">
+                        {/* <svg
+                          class="Icon Icon--minus"
+                          role="presentation"
+                          viewBox="0 0 16 2"
+                        >
+                          <path
+                            d="M1,1 L15,1"
+                            stroke="currentColor"
+                            fill="none"
+                            fill-rule="evenodd"
+                            stroke-linecap="square"
+                          ></path>
+                        </svg> */}
+                      </span>
+                      <input
+                        type="text"
+                        className="quantitySelector__currentQuantity"
+                      />
+                      <span className="quantitySelector__btn increase-quantity">
+                        {/* <svg
+                          class="Icon Icon--plus"
+                          role="presentation"
+                          viewBox="0 0 16 16"
+                        >
+                          <g
+                            stroke="currentColor"
+                            fill="none"
+                            fill-rule="evenodd"
+                            stroke-linecap="square"
+                          >
+                            <path d="M8,1 L8,15"></path>
+                            <path d="M1,8 L15,8"></path>
+                          </g>
+                        </svg> */}
+                      </span>
                     </div>
                   </div>
+                </div>
+                <button
+                  type="submit"
+                  className="productForm__addToCart Button button-full button-primary"
+                >
+                  Add to cart
+                </button>
+              </form>
+              <div className="product__quickNav">
+                <div className="product__quickNavWrapper">
+                  <Link className="Heading link--secondary">
+                    More information
+                  </Link>
+                  <svg className="icon icon--selet-arrow-right">
+                    <path
+                      d="M1.5 1.5l8 7.5-8 7.5"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      fill-rule="evenodd"
+                      stroke-linecap="square"
+                    ></path>
+                  </svg>
                 </div>
               </div>
             </div>
