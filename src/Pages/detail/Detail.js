7d73d5a667d23eab8ed56f8cb319662e2aaa6edb
daseulsong <vates0328@gmail.com>
2020-08-24 21:49:23 +0900

    [송다슬] Add: 상세페이지에 Nav와 Footer 결합

--- src/Pages/detail/Detail.js
+++ src/Pages/detail/Detail.js
@@ -1,36 +1,40 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./Detail.scss";
+import Nav from "../../Components/nav/Nav";
 import ProductSlide from "../detail/Components/productSlide/ProductSlide";
 import ProductInfo from "../detail/Components/productInfo/ProductInfo";
 import ProductAside from "../detail/Components/productAside/ProductAside";
 import Footer from "../../Components/footer/Footer";
+import "./Detail.scss";
 
 class Detail extends Component {
   render() {
     return (
-      <main>
-        <div className="productLarge product">
-          <div className="product__wrapper">
-            <div className="product__display--withDots">
-              <span className="anchor"></span>
-              <div className="product__slideNav">
-                <div className="product__slideNav--scroller">
-                  <Link
-                    className="navDot is-selected"
-                    to="/shop/detail/"
-                  ></Link>
-                  <Link className="navDot" to="/shop/detail/"></Link>
+      <div className="Detail">
+        <Nav />
+        <main>
+          <div className="productLarge product">
+            <div className="product__wrapper">
+              <div className="product__display--withDots">
+                <span className="anchor"></span>
+                <div className="product__slideNav">
+                  <div className="product__slideNav--scroller">
+                    <Link
+                      className="navDot is-selected"
+                      to="/shop/detail/"
+                    ></Link>
+                    <Link className="navDot" to="/shop/detail/"></Link>
+                  </div>
                 </div>
+                <ProductSlide />
               </div>
-              <ProductSlide />
             </div>
+            <ProductInfo />
+            <ProductAside />
           </div>
-          <ProductInfo />
-          <ProductAside />
-        </div>
-        <Footer />
-      </main>
+          <Footer />
+        </main>
+      </div>
     );
   }
 }
