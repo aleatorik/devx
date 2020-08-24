81653b83c1a7b62004c8769028a34c85395e4983
lhh <jyslshsgdy@gmail.com>
2020-08-24 22:16:21 +0900

    [Modify]: Account.js, Button.js, Input.js 수정 - fetch로 로그인 기능 추가 - Account.js에서 Input, Button에 eventHandler 전달 - 전달 받은 eventHandler로 Account.js state 관리

--- src/Components/button/Button.js
+++ src/Components/button/Button.js
@@ -2,43 +2,12 @@ import React, { Component } from 'react';
 import './Button.scss';
 
 class Button extends Component {
-    handleOnclick = (e) => {
-        // if(e.target.name === "login"){
-        //     fetch('http://10.58.5.34:8000/account/sign_in', {
-        //         method: 'POST',
-        //         body: JSON.stringify({
-        //             email: idPw.idInput,
-        //             pw: idPw.pwInput
-        //         })
-        //     }).then(response => response.json())
-        //     .then(response => {
-        //         localStorage.setItem('token', response.Your_token);
-        //         console.log(response);
-        //     });
-        // }
-        // else{
-        //     fetch('http://10.58.5.34:8000/account/sign_in', {
-        //         method: 'POST',
-        //         body: JSON.stringify({
-        //             name: idPw.idInput,
-        //             lname: idPw.pwInput,
-        //             email: 'qwer@gmail.com',
-        //             pw: '1234-1234'
-        //         })
-        //     }).then(response => response.json())
-        //     .then(response => {
-        //         localStorage.setItem('token', response.Your_token);
-        //         console.log(response);
-        //     });
-        // }
-    };
-
     render() {
-        const {name, text} = this.props;
+        const {text, handleOnclick} = this.props;
 
         return (
             <div className="ButtonContainer">
-                <button name={name} onClick={this.handleOnclick}>{text}</button>
+                <button onClick={handleOnclick}>{text}</button>
             </div>
         );
     }
--- src/Pages/account/Account.js
+++ src/Pages/account/Account.js
@@ -7,24 +7,45 @@ import Nav from '../../Components/nav/Nav';
 import Footer from '../../Components/footer/Footer';
 
 class Account extends Component {
-    render() {
+    state = {
+        email: '',
+        password: ''
+    };
+
+    handleOnclick = (e) => {
+        e.preventDefault();
+        fetch('http://10.58.5.213:8000/sign/in', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: this.state.email,
+                password: this.state.password
+            })
+        }).then(response => response.json())
+        .then(response => {
+            console.log(response);
+        });
+    }
+
+    handleOnChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {console.log(this.state)
         return (
             <div className="AccountContainer">
-                <Nav />
                 <form className="accountForm">
                     <div className="loginText">LOGIN</div>
                     <div>
                         <span>Please enter your e-mail and password:</span>
                     </div>
-                    <Input placeHolder="Email" />
-                    <Input placeHolder="Password" forgotPw={true}/>
-                    <Button name="login" text="LOGIN" />
+                    <Input name="email" handleOnChange={this.handleOnChange} placeHolder="Email" />
+                    <Input name="password" handleOnChange={this.handleOnChange} placeHolder="Password" forgotPw={true}/>
+                    <Button handleOnclick={this.handleOnclick} text="LOGIN" />
                     <div className="accountBottom">
                         <span>Don't have an account? </span>
                         <Link to="register"><span className="goToRegister">Create one</span></Link>
                     </div>
                 </form>
-                <Footer />
             </div>
         );
     }
