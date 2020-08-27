7f447219702d281fcf32bbc566b2c3cfb35b9f03
daseulsong <vates0328@gmail.com>
2020-08-27 18:48:08 +0900

    [송다슬] Add: 장바구니, 회원가입 라우트 추가

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Falcon9 from "./Pages/falcon9/Falcon9";
 import Main from "./Pages/main/Main";
 import Shop from "./Pages/shop/Shop";
-// import Detail from "./Pages/detail/Detail";
+import Detail from "./Pages/detail/Detail";
+import Cart from "./Pages/cart/Cart";
+import Register from "./Pages/register/Register";
+import Account from "./Pages/account/Account";
 
 class Routes extends React.Component {
   render() {
@@ -13,7 +16,10 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/falcon9" component={Falcon9} />
           <Route exact path="/shop" component={Shop} />
-          {/* <Route exact path="/shop/detail" component={Detail} /> */}
+          <Route exact path="/shop/detail" component={Detail} />
+          <Route exact path="/shop/cart" component={Cart} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/account" component={Account} />
         </Switch>
       </Router>
     );
