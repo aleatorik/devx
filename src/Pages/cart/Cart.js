6cdc2f98f49b9c01b45e19818f03fbe145e01e4a
lhh <jyslshsgdy@gmail.com>
2020-08-28 05:29:03 +0900

    [Modify]: 장바구니 페이지 수정, footer 수정 - 장바구니 페이지 수량 수정시 가격 변화 - 장바구니 페이지 상품 삭제 기능 - footer css 수정

--- src/Pages/cart/Cart.js
+++ src/Pages/cart/Cart.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import CartList from './component/CartList';
 import './Cart.scss';
 import List from './data.json';
+import Nav from '../../Components/nav/Nav';
+import Footer from '../../Components/footer/Footer';
 
 class Cart extends Component {
     state = {
-        cartList: []
+        cartList: [],
+        totalPrice: 0
     };
 
     componentDidMount(){
-        this.setState({cartList: List.product});
+        const list = List.product;
+
+        this.setState({
+            cartList: list,
+            totalPrice: list.reduce((total, el) => total += (el.count * el.price.substr(1).split('.')[0]), 0)
+        });
     }
 
-    handleClick = (index, str) => {
+    handleClickCount = (index, str) => {
         if(str.includes("plus")){
             const selectedObj = this.state.cartList.find(el => el.id === index);
             selectedObj.count++;
@@ -20,7 +28,10 @@ class Cart extends Component {
             const arrCopy = [ ...this.state.cartList ];
             arrCopy[index] = selectedObj;
     
-            this.setState({ cartList: arrCopy });
+            this.setState({ 
+                cartList: arrCopy,
+                totalPrice: arrCopy.reduce((total, el) => total += (el.count * el.price.substr(1).split('.')[0]), 0)
+            });
         }
         else{
             const selectedObj = this.state.cartList.find(el => el.id === index);
@@ -32,39 +43,52 @@ class Cart extends Component {
             const arrCopy = [ ...this.state.cartList ];
             arrCopy[index] = selectedObj;
     
-            this.setState({ cartList: arrCopy });
+            this.setState({ 
+                cartList: arrCopy,
+                totalPrice: arrCopy.reduce((total, el) => total += (el.count * el.price.substr(1).split('.')[0]), 0)
+            });
         }
     }
 
-    handleRemoveClick = () => {
-        
+    handleRemoveClick = (index) => {
+        this.setState({
+            cartList: this.state.cartList.filter(el => el.id !== index)
+        })
     }
     
     render() {
+        const {totalPrice} = this.state;
+
         return (
-            <div className="CartContainer">
-                <div className="txtCartContainer">
-                    <div className="txtCart">CART</div>
-                </div>
-                <div className="txtContainer">
-                    <span className="txtProduct">PRODUCT</span>
-                    <span></span>
-                    <span className="txtQuantity">QUANTITY</span>
-                    <span className="txtTotal">TOTAL</span>
-                </div>
-                <div className="cartListContainer">
-                    {this.state.cartList.map((el, index) => 
-                        <CartList key={index} img={el.img} productName={el.productName} size={el.size} price={el.price} index={index} count={el.count} handleClick={this.handleClick} />
-                    )}
-                </div>
-                <div className="totalContainer">
-                    <div className="txtTotalPrice">TOTAL: $100.00</div>
-                    <div className="shipping">Shipping & taxes calculated at checkout</div>
-                    <div className="totalBtnContainer">
-                        <button className="btnCheckout">CHECKOUT</button>
+            <>
+            <div className="Cart">
+                <Nav />
+                <div className="cartContainer">
+                    <div className="txtCartContainer">
+                        <div className="txtCart">CART</div>
+                    </div>
+                    <div className="txtContainer">
+                        <span className="txtProduct">PRODUCT</span>
+                        <span></span>
+                        <span className="txtQuantity">QUANTITY</span>
+                        <span className="txtTotal">TOTAL</span>
+                    </div>
+                    <div className="cartListContainer">
+                        {this.state.cartList.map((el, index) => 
+                            <CartList key={index} handleClickCount={this.handleClickCount} handleRemoveClick={this.handleRemoveClick} {...el}/>
+                        )}
+                    </div>
+                    <div className="totalContainer">
+                        <div className="txtTotalPrice">{`TOTAL: $${totalPrice}.00`}</div>
+                        <div className="shipping">Shipping & taxes calculated at checkout</div>
+                        <div className="totalBtnContainer">
+                            <button className="btnCheckout">CHECKOUT</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <Footer />
+        </>
         );
     }
 }
--- src/Pages/cart/component/CartList.js
+++ src/Pages/cart/component/CartList.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import './CartList.scss';
 
 class CartList extends Component {
+    state = {
+        priceState: 0
+    };
+
+    componentDidMount(){
+        const {price} = this.props;
+
+        this.setState({
+            priceState: price.substr(1).split('.')[0]
+        })
+    }
+
     render() {
-        const {img, productName, size, price, index, count, handleClick} = this.props;
+        const {id, img, productName, color, size, price, count, index, handleClickCount, handleRemoveClick} = this.props;
+        const {priceState} = this.state;
 
         return (
             <div className="CartListContainer">
                 <img className="productImg" src={img}/>
                 <div className="productInfo">
                     <p className="productInfoDetail">{productName}</p>
-                    <p className="productInfoDetail">{size}</p>
+                    <p className="productInfoDetail">{color} / {size}</p>
                     <p className="productInfoDetail">{price}</p>
                 </div>
                 <div className="productQuantity">
                     <div className="productQuantityCounter">
-                        <div onClick={() => handleClick(index, "minus")} className="counterEl">ㅡ</div>
+                        <div onClick={() => handleClickCount(id, "minus")} className="counterEl">ㅡ</div>
                         <div className="counterEl">{count}</div>
-                        <div onClick={() => handleClick(index, "plus")} className="counterEl">＋</div>
+                        <div onClick={() => handleClickCount(id, "plus")} className="counterEl">＋</div>
                     </div>
-                    <button className="quantityBtn">REMOVE</button>
+                    <button onClick={() => handleRemoveClick(id)} className="quantityBtn">REMOVE</button>
                 </div>
-                <div className="totalPrice">$20.00</div>
+                <div className="totalPrice">{`$${priceState * count}.00`}</div>
             </div>
         );
     }
