223134c44975cce9c6cd9b0e5dabf6a7d0a8d86d
daseulsong <vates0328@gmail.com>
2020-08-28 12:39:49 +0900

    [송다슬] Conflict 해결

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,9 @@ import Falcon9 from "./Pages/falcon9/Falcon9";
 import Main from "./Pages/main/Main";
 import Shop from "./Pages/shop/Shop";
 import Detail from "./Pages/detail/Detail";
-// import Cart from "./Pages/cart/Cart";
+import Cart from "./Pages/cart/Cart";
 import Register from "./Pages/register/Register";
 import Account from "./Pages/account/Account";
-
-// "/shop/detail/:id"
 
 class Routes extends React.Component {
   render() {
@@ -19,7 +17,7 @@ class Routes extends React.Component {
           <Route exact path="/falcon9" component={Falcon9} />
           <Route exact path="/shop" component={Shop} />
           <Route exact path="/shop/detail/" component={Detail} />
-          {/* <Route exact path="/shop/cart" component={Cart} /> */}
+          <Route exact path="/shop/cart" component={Cart} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/account" component={Account} />
         </Switch>
